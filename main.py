2adb8684d257a67017633dfda518de2134b1fb21
melisk12 <129781468+melisk12@users.noreply.github.com>
2023-12-28 14:46:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ sahip olursun. Bu tatil öneri sistemi, tatil planlama sürecini daha keyifli bi
 iyi tatil deneyimini yaşama fırsatı sunar. Hayalini kurduğun tatilin kapılarını aralamak için bu sistemi kullanarak, unutulmaz bir kaçışa adım atabilirsiniz.""")
 
 # Dosya yolu
-image_path = "media/1.jpg"
+image_path = "1.jpg"
 # Dosyayı göster
 columns_2.image(image_path, use_column_width=True)
 
@@ -35,50 +35,50 @@ columns_a, columns_b, columns_c, columns_d = tab_write.columns(4)
 
 #columns_a 1. yazı
 columns_a.subheader("Almanya'nın masalsı kasabası: Monschau")
-image_path = "media/monschau.jpg"
+image_path = "monschau.jpg"
 columns_a.image(image_path, use_column_width=True)
 columns_a.markdown("Ortaçağ'dan kalma ve çağının özelliklerini yansıtan kasaba, yarı ahşap evleri, arnavut kaldırımlı dar sokakları, yemyeşil doğasıyla, bulunduğu Eifel bölgesinin en popüler seyahat rotalarından biri. Özellikle yürüyüş ve bisiklet tutkunlarını mutlu eden kasabanın çevresindeki yürüyüş parkurları ve bisiklet yollarında, mevsiminde nergis çiçeklerinin kokusu, kayın ormanlarının heybeti ziyaretçilerine eşlik ediyor.")
 
 #2. yazı
 columns_a.subheader("İstanbul'dan kopamayanlara baharı karşılamak için ATATÜRK ARBORETUMU")
-image_path = "media/Ataturk-Arboretumu.jpg"
+image_path = "Ataturk-Arboretumu.jpg"
 columns_a.image(image_path, use_column_width=True)
 columns_a.markdown("Kent hayatından sıkılıp baharda doğaya kaçmak istiyor ama işten, güçten dolayı şehir dışına çıkamıyorsanız doğru adres Sarıyer. 1949’dan beri bu güzelliğin farkında olan İstanbullular nefes almak için burayı tercih ediyor. Göller, bitkiler, ördekler, kazlar, sukaplumbağaları... Ağaçlar ve çiçekler arasında yürüyüş yapmak harika.")
 
 #columns_b yazı 1
 columns_b.subheader("Bu sene yeni yılda nereye gitsek ? Avrupa’nın en güzel Noel Pazarları")
-image_path = "media/noel.jpg"
+image_path = "noel.jpg"
 columns_b.image(image_path, use_column_width=True)
 columns_b.markdown("Yeni yılın yaklaşmasıyla birlikte birçok Avrupa kentinde ışıl ışıl renkli ve bir o kadar da canlı Noel pazarları kurulmaya başladı. Eğlencenin, yemeğin ve alışverişin zirveye çıktığı pazarlar arasında yüzlerce yıldır aynı yerde kurulanı da var, etkileyici şovlara ev sahipliği yapanı da. Yılın ilk günlerini karşılamak için süslenmiş, coşku dolu şehirlerde yeni yıla girmek unutulmaz bir deneyime dönüşebilir.")
 
 #columns_b yazı 2
 columns_b.subheader("Parkın içinde bir bir şehir: Kiev")
-image_path = "media/kiev.jpg"
+image_path = "kiev.jpg"
 columns_b.image(image_path, use_column_width=True)
 columns_b.markdown("Dünyaca tanınan mimari yapıtları, keyifli bahçeleri, yemyeşil ve büyük parkları ile insanı büyüleyen bir şehir Kiev. Goethe'nin parkın içindeki şehir olarak anlattığı Kiev kimilerinin unutulmaz anılar biriktirdiği kimilerinin ise tarihi yapılarına hayran kaldığı muazzam şehir.")
 
 #columns_c yazı 1
 columns_c.subheader("Özgün kültür, bin yıllık tapınaklar; Siem Reap")
-image_path = "media/siem_reap.jpg"
+image_path = "siem_reap.jpg"
 columns_c.image(image_path, use_column_width=True)
 columns_c.markdown("10 yıl öncesine kadar Siem Reap, 12’nci yüzyıldan kalma Angkor Wat Tapınakları’nı görmek için gidenlerin konakladığı, yemek yediği sıradan bir yerleşimdi. Şimdi modern otelleri, iddialı restoranları, sanat galerileri, fotoğraf ve film festivalleri, özgün ürünlerin satıldığı mağazalarıyla başlı başına bir çekim merkezi.")
 
 #columns_c yazı 2
 columns_c.subheader("Doğanın ışıkla dansı, görsel şölen Kuzey Işıkları...")
-image_path = "media/lotofen.jpg"
+image_path = "lotofen.jpg"
 columns_c.image(image_path, use_column_width=True)
 columns_c.markdown("Başta Jules Verne olmak üzere yazarlar ve ressamlara ilham olan Lofoten Adaları'nın en güzel özelliklerinden birinin Kuzey Kutup Daire’sine yakın olması nedeniyle oluşan Gece yarısı güneşi olduğu biliniyor. Gece yarısı güneşi sonucu ortaya çıkan manzarayı seyretmenin keyfi ise eşsiz! Aynı zamanda bu manzara karşısında kayaking, balık tutma, bisiklet sürme gibi aktiviteler yapabilir ya da sadece bu güzel manzarayı izleyebilirsiniz.")
 
 
 #columns_d yazı 1
 columns_d.subheader("Dünyanın En Renkli Festivali; Holi Festivali")
-image_path = "media/holi.jpg"
+image_path = "holi.jpg"
 columns_d.image(image_path, use_column_width=True)
 columns_d.markdown("Dünyanın en renkli ve eğlenceli festivallerinden biri olan Holi Festivali (Holi Fest) yani Renklerin Festivali, baharın gelişini kutlayan, çok renkliliği ve yeniden doğuşu simgeleyen bir Hinduizm renk festivali. Havanın ısınmasının, doğanın uykusundan uyanmasının, bitkilerin tomurcuklanıp çiçeklenmesinin müjdecisi olarak kutlanıyor. Holi festivalinin en eğlenceli kısmı ise halkın rengârenk toprak boyaları önce yüzlerine sürüp sonra etrafındakilerin üzerine atıyor olması.")
 
 #columns_d yazı 2
 columns_d.subheader("Kışın bir başka karlar altında Kopenhag")
-image_path = "media/kopenhag.jpg"
+image_path = "kopenhag.jpg"
 columns_d.image(image_path, use_column_width=True)
 columns_d.markdown("Bu şehri kışın ziyaret edeceksen huzurlu ve refah dolu bu kenti kışın ziyaret etmenin keyfi bir başka. Kopenhag’ın en güzel yerlerinden biri olan Kastelette. Yıldız şeklindeki yapısı ise ve Ortaçağ Kaleleri gibi etrafını dolduran iki kat hendeği, yemyeşil çevresi ve içindeki kiremit rengi binaları ile Andersen Masalları’ndan fırlamış gibi.")
 #tab_model
